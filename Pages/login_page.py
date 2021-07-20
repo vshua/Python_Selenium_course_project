ecadb9ddc52a77479ab9686718ff24ec1ea33b3b
VSh <volodymyr.shevchuk@atola.com>
2021-07-20 09:54:14 +0300

    Tests for Login Page + login page obj

---
+++ Pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        current_url = self.browser.current_url
+        assert "login" in str(current_url), "Login is not presented in the link"
+        assert True
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert True
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert True
